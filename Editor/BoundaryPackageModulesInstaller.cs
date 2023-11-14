caf5141760e892ad6d127a7b8545a7a7ac014fe5
Dino Fejzagić <dino.f@live.de>
2023-11-14 18:50:07 +0100

    Core transfer of boundary service

--- Editor/BoundaryPackageModulesInstaller.cs
+++ Editor/BoundaryPackageModulesInstaller.cs
@@ -5,7 +5,6 @@ using RealityCollective.ServiceFramework.Definitions;
 using RealityCollective.ServiceFramework.Editor.Packages;
 using RealityCollective.ServiceFramework.Services;
 using RealityToolkit.Boundary.Definitions;
-using RealityToolkit.Boundary.Interfaces;
 using System.Linq;
 using UnityEditor;
 
@@ -39,7 +38,7 @@ namespace RealityToolkit.Boundary.Editor
         /// <inheritdoc/>
         public bool Install(ServiceConfiguration serviceConfiguration)
         {
-            if (!typeof(IBoundaryModule).IsAssignableFrom(serviceConfiguration.InstancedType.Type))
+            if (!typeof(IBoundaryServiceModule).IsAssignableFrom(serviceConfiguration.InstancedType.Type))
             {
                 // This module installer does not accept the configuration type.
                 return false;
@@ -57,14 +56,14 @@ namespace RealityToolkit.Boundary.Editor
                 return false;
             }
 
-            if (!ServiceManager.Instance.TryGetServiceProfile<IBoundary, BoundaryProfile>(out var BoundaryProfile))
+            if (!ServiceManager.Instance.TryGetServiceProfile<IBoundaryService, BoundaryServiceProfile>(out var BoundaryProfile))
             {
                 UnityEngine.Debug.LogWarning($"Could not install {serviceConfiguration.InstancedType.Type.Name}.{nameof(BoundaryProfile)} not found.");
                 return false;
             }
 
             // Setup the configuration.
-            var typedServiceConfiguration = new ServiceConfiguration<IBoundaryModule>(serviceConfiguration.InstancedType.Type, serviceConfiguration.Name, serviceConfiguration.Priority, serviceConfiguration.RuntimePlatforms, serviceConfiguration.Profile);
+            var typedServiceConfiguration = new ServiceConfiguration<IBoundaryServiceModule>(serviceConfiguration.InstancedType.Type, serviceConfiguration.Name, serviceConfiguration.Priority, serviceConfiguration.RuntimePlatforms, serviceConfiguration.Profile);
 
             // Make sure it is not already in the target profile.
             if (BoundaryProfile.ServiceConfigurations.All(sc => sc.InstancedType.Type != serviceConfiguration.InstancedType.Type))
